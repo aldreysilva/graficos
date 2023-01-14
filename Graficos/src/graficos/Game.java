5dfee86225ca80661b4bf4b12b2981ce737be7fd
aldreysilva <94716967+aldreysilva@users.noreply.github.com>
2023-01-13 21:21:25 -0300

    carregando string
    carregando string

---
+++ Graficos/src/graficos/Game.java
@@ -0,0 +1,115 @@
+package graficos;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	public static JFrame frame;
+	private Thread thread;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+	boolean isRunning = true;
+	private BufferedImage image;
+	public Game() {
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+	}
+
+	public void initFrame() {
+		frame = new JFrame("Game 1");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+
+	}
+
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+
+	public void tick() {
+
+	}
+
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(Color.GREEN);
+		g.fillRect(0,0,WIDTH,HEIGHT);
+		
+		g.setFont(new Font("Aril", Font.BOLD,20));
+		g.setColor(Color.yellow);
+		g.drawString("ola mundo" , 20, 20 );
+		
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
+	}
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOFTicks = 60.0;
+		double ns = 1000000000 / amountOFTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+
+		while (isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+
+			}
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS:" + frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
+		stop();
+
+	}
+
+}
